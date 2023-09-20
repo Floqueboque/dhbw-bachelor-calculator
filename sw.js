aa194d29ff6f51b4ce8b33bcb7ecc5a7b35fc82b
Florian Herkommer <35303060+Floqueboque@users.noreply.github.com>
2023-09-20 08:02:38 +0000

    updated version

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/dhbw-bachelor-calculator';
 var APP_PREFIX = 'dhbwbc_';
-var VERSION = 'version_003';
+var VERSION = 'version_004';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
